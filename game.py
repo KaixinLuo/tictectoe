48905a07e205ac0119fe84739df3c8e6cc5cc642
KaixinLuo <704556595@qq.com>
2018-05-29 22:26:07 -0400

    commit

---
+++ game.py
@@ -0,0 +1,6 @@
+import core
+
+class game:
+    def __init__(self):
+        self.board = core.new_board()
+        self.
---
+++ player.py
@@ -0,0 +1,6 @@
+import ai
+import core 
+
+class human_player(object):
+    def __init__ (self,label):
+        self.label = label

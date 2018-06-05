7a90dc09657aa760da26c9ae87b1a780d4da9291
Wally Yang <wallyyang28@gmail.com>
2018-06-05 17:15:11 +0000

    test: test ai functions

---
+++ test.py
@@ -0,0 +1,38 @@
+import core
+import ai
+
+def test_available_ply():
+    board1 = core.new_board()
+    result1 = core.available_ply(board1)
+
+    print(result1)
+
+def test_utility():
+    board1 = [1, 1, 1, -1, -1, 0, 0, 0, 0]
+    result1 = ai.utility(board1)
+    print(result1 == 10)
+
+    board2 = [-1, -1, -1, 0, 1, 0, 1, 1, 0]
+    result2 = ai.utility(board2)
+    print(result2 == -10)
+
+    board3 = [-1, 1, -1, 1, 1, 0, 1, 0, 1]
+    result3 = ai.utility(board3)
+    print(result3 == 0)
+
+def test_max_utility():
+    board1 = [1, 1, 1, -1, -1, 0, 0, 0, 0]
+    result1 = ai.max_utility(board1)
+    print(result1 == 10)
+
+    board2 = [-1, -1, -1, 0, 1, 0, 1, 1, 0]
+    result2 = ai.max_utility(board2)
+    print(result2 == -10)
+
+    board3 = [-1, 1, -1, 1, 1, 0, 1, 0, 1]
+    result3 = ai.max_utility(board3)
+    print(result3 == 0)   
+
+if __name__ == '__main__':
+    test_utility()
+    test_max_utility()

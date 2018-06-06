e0b70acabdbc25b8f458cf01b63df3dcc291986c
Wally Yang <wallyyang28@gmail.com>
2018-06-06 15:48:12 +0000

    test: add max utility test cases

--- test.py
+++ test.py
@@ -22,17 +22,25 @@ def test_utility():
 
 def test_max_utility():
     board1 = [1, 1, 1, -1, -1, 0, 0, 0, 0]
-    result1 = ai.max_utility(board1)
+    result1 = ai.max_utility(board1, 1)
     print(result1 == 10)
 
     board2 = [-1, -1, -1, 0, 1, 0, 1, 1, 0]
-    result2 = ai.max_utility(board2)
+    result2 = ai.max_utility(board2, 1)
     print(result2 == -10)
 
     board3 = [-1, 1, -1, 1, 1, 0, 1, 0, 1]
-    result3 = ai.max_utility(board3)
+    result3 = ai.max_utility(board3, 1)
     print(result3 == 0)   
+
+    board4 = [1, 1, 0, 0, -1, 0, 0, 0, -1]
+    result4 = ai.max_utility(board4, 1)
+    print(result4 == 10)
+
+def test_min_utility():
+    pass
 
 if __name__ == '__main__':
     test_utility()
     test_max_utility()
+    test_min_utility()

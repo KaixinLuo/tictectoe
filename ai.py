f438056aaf19ce8b454adc839235cef53a02a9ce
Wally Yang <wallyyang28@gmail.com>
2018-06-10 16:17:56 +0000

    style: remove obsolete commented code

--- ai.py
+++ ai.py
@@ -1,25 +1,9 @@
 import math
 import core
 
-
 def available_policy(board):
     return [x for x in range(9) if board[x]==0]
 
-# this is not correct
-'''
-def get_the_best_policy(board,player_flag,opt_func = lambda x : max(x),):
-    possible_policies = get_avaliable_policy(board)
-    result = []
-    for policy in possible_policies:
-        new_board = update_board(board,policy)
-        (has_empty_position,winner) = get_game_state(new_board)
-        if ((not has_empty_position) or winner != 0):
-            result.append((policy,winner*10/player_flag))
-        else:
-            (p,r)=get_the_best_policy(new_board,min if opt_func==max else max,player_flag )
-            result.append((policy,r))
-    return None if result == [] else opt_func(result,key=lambda x: x[1])
-'''
 def best_policy_and_util(board):
     possible_policies =available_policy(board)
     best_policy = -1
@@ -106,5 +90,3 @@ def utility(board):
         return -10
     else:
         return 0
-
-

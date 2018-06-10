49de93672bb8313c09137e3c0842ada5b1bf0d19
罗恺欣(Ken) <704556595@qq.com>
2018-06-09 21:05:19 -0400

    ''

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Bot:
     
     def play(board):
         own_vision=[i/self.label for i in board]
-        (policy,util)=best_policy_and_util(own_vision)
+        (policy,util)=ai.best_policy_and_util(own_vision)
         board[policy]=self.label
 
 class Human(object):
